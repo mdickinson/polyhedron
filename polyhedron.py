f19f4213693cdccedbd1b2c244d4e9e2e06104c6
Mark Dickinson <dickinsm@gmail.com>
2014-06-21 08:51:28 +0100

    Attempt to streamline computation; partial work.

--- polyhedron.py
+++ polyhedron.py
@@ -3,114 +3,12 @@ Class representing a Polyhedron, described by a triangular mesh.
 
 """
 
-
 def sign(x):
     """
     Return 1 if x is positive, -1 if it's negative, and 0 if it's zero.
 
     """
     return (x > 0) - (x < 0)
-
-
-#### Geometric primitives #####################################################
-
-def ccw2(p1, p2, p3):
-    """
-    Determine whether the line from p1 to p2 passes to the left or right of p3.
-
-    Return 1 if p1-p2-p3 represents a counterclockwise turn,
-    -1 if p1-p2-p3 represents a clockwise turn, and 0 if the three
-    points are collinear.
-
-    """
-    det = (p1[0] - p3[0]) * (p2[1] - p3[1]) - (p1[1] - p3[1]) * (p2[0] - p3[0])
-    return sign(det)
-
-
-def vertex_sign(v1, origin):
-    """
-    Sign of the vertex v1, relative to the origin.
-
-    """
-    return (sign(v1[0] - origin[0]) or
-            sign(v1[1] - origin[1]) or
-            sign(v1[2] - origin[2]))
-
-
-def edge_sign(v1, v2, origin):
-    """
-    Sign of the edge from v1 to v2, relative to origin.
-
-    """
-    d = (ccw2(v1, v2, origin) or
-         ccw2((v1[0], v1[2]), (v2[0], v2[2]), (origin[0], origin[2])) or
-         ccw2((v1[1], v1[2]), (v2[1], v2[2]), (origin[1], origin[2])))
-    return d
-
-
-def triangle_sign(p1, p2, p3, p4):
-    """
-    Determine whether the counterclockwise triangle p1-p2-p3
-    has normal pointing away or towards p4.
-
-    Return 1 if the normal points away from p4, -1 if
-    it points towards p4, and 0 if p4 is *contained*
-    in the plane of the triangle p1-p2-p3.
-
-    """
-    # We're computing the determinant of the matrix
-    #
-    #    ( p1[0]  p2[0]  p3[0]  p4[0] )
-    #    ( p1[1]  p2[1]  p3[1]  p4[1] )
-    #    ( p1[2]  p2[2]  p3[2]  p4[2] )
-    #    (  1      1       1       1  )
-
-    # Minors for 3rd row.
-    m1 = (p2[0] - p4[0]) * (p3[1] - p4[1]) - (p2[1] - p4[1]) * (p3[0] - p4[0])
-    m2 = (p1[0] - p4[0]) * (p3[1] - p4[1]) - (p1[1] - p4[1]) * (p3[0] - p4[0])
-    m3 = (p1[0] - p4[0]) * (p2[1] - p4[1]) - (p1[1] - p4[1]) * (p2[0] - p4[0])
-    m4 = (p1[0] - p3[0]) * (p2[1] - p3[1]) - (p1[1] - p3[1]) * (p2[0] - p3[0])
-
-    return sign(p1[2] * m1 - p2[2] * m2 + p3[2] * m3 - p4[2] * m4)
-
-
-#### Homology computations ####################################################
-
-def vertex_chain(v1, origin):
-    """
-    Map the vertex v1 to the corresponding 0-chain in cellular homology.
-
-    Return coefficient of [F] in C_0.
-
-    Raise ValueError if v1 == origin.
-
-    """
-    d = vertex_sign(v1, origin)
-    if not d:
-        raise ValueError("Vertex contains origin")
-    # [F] if d > 0, else [B].
-    return 1 if d > 0 else 0
-
-
-def edge_chain(v1, v2, origin):
-    """
-    Map the edge v1-v2 to the corresponding 1-chain in cellular homology.
-
-    Return coefficient of [R] in C_1 = Z + Z.
-
-    Raise ValueError if the edge goes through the origin.
-
-    """
-    edge_boundary = vertex_chain(v2, origin) - vertex_chain(v1, origin)
-    if not edge_boundary:
-        return 0
-
-    d = edge_sign(v1, v2, origin)
-    if not d:
-        raise ValueError("Edge contains origin")
-
-    # d * [L] or d * [R], depending on sign of d * edge_boundary.
-    return d if d * edge_boundary < 0 else 0
 
 
 def triangle_chain(v1, v2, v3, origin):
@@ -123,19 +21,85 @@ def triangle_chain(v1, v2, v3, origin):
     Raise ValueError if the face contains the origin.
 
     """
-    face_boundary = sum(
-        edge_chain(start, end, origin)
-        for start, end in [(v1, v2), (v2, v3), (v3, v1)])
+    # Result depends on various minors of the matrix:
+    #
+    # ( v1[0] v2[0] v3[0] origin[0] )
+    # ( v1[1] v2[1] v3[1] origin[1] )
+    # ( v1[2] v2[2] v3[2] origin[2] )
+    # (   1     1     1      1      )
+
+    m1_0 = v1[0] - origin[0]
+    m1_1 = v1[1] - origin[1]
+    m1_2 = v1[2] - origin[2]
+    if m1_0 == m1_1 == m1_2 == 0:
+        raise ValueError("v1 == origin")
+    v1positive = m1_0 > 0 or m1_0 == 0 and (m1_1 > 0 or m1_1 == 0 and m1_2 > 0)
+
+    m2_0 = v2[0] - origin[0]
+    m2_1 = v2[1] - origin[1]
+    m2_2 = v2[2] - origin[2]
+    if m2_0 == m2_1 == m2_2 == 0:
+        raise ValueError("v2 == origin")
+    v2positive = m2_0 > 0 or m2_0 == 0 and (m2_1 > 0 or m2_1 == 0 and m2_2 > 0)
+
+    m3_0 = v3[0] - origin[0]
+    m3_1 = v3[1] - origin[1]
+    m3_2 = v3[2] - origin[2]
+    if m3_0 == m3_1 == m3_2 == 0:
+        raise ValueError("v3 == origin")
+    v3positive = m3_0 > 0 or m3_0 == 0 and (m3_1 > 0 or m3_1 == 0 and m3_2 > 0)
+
+    if v1positive == v2positive == v3positive:
+        return 0
+
+    # Look for positive edges from N to P,
+    # and negative edges from P to N.
+    face_boundary = 0
+
+    edge_boundary = v2positive - v1positive
+    m12_01 = d1 = m1_0 * m2_1 - m1_1 * m2_0
+    if edge_boundary:
+        # More minors: m12_01, m12_02, m12_12
+        m12_02 = d2 = m1_0 * m2_2 - m1_2 * m2_0
+        m12_12 = d3 = m1_1 * m2_2 - m1_2 * m2_1
+        if m12_01 == m12_02 == m12_12 == 0:
+            raise ValueError("v1, v2, origin collinear")
+        e12_positive = d1 < 0 or d1 == 0 and (d2 < 0 or d2 == 0 and d3 < 0)
+        if e12_positive == v2positive:
+            face_boundary += edge_boundary
+
+    edge_boundary = v3positive - v2positive
+    m23_01 = d1 = m2_0 * m3_1 - m2_1 * m3_0
+    if edge_boundary:
+        m23_02 = d2 = m2_0 * m3_2 - m2_2 * m3_0
+        m23_12 = d3 = m2_1 * m3_2 - m2_2 * m3_1
+        if d1 == d2 == d3 == 0:
+            raise ValueError("v2, v3, origin collinear")
+        e23_positive = d1 < 0 or d1 == 0 and (d2 < 0 or d2 == 0 and d3 < 0)
+        if e23_positive == v3positive:
+            face_boundary += edge_boundary
+
+    edge_boundary = v1positive - v3positive
+    m31_01 = d1 = m3_0 * m1_1 - m3_1 * m1_0
+    if edge_boundary:
+        m31_02 = d2 = m3_0 * m1_2 - m3_2 * m1_0
+        m31_12 = d3 = m3_1 * m1_2 - m3_2 * m1_1
+        if d1 == d2 == d3 == 0:
+            raise ValueError("v3, v1, origin collinear")
+        e31_positive = d1 < 0 or d1 == 0 and (d2 < 0 or d2 == 0 and d3 < 0)
+        if e31_positive == v1positive:
+            face_boundary += edge_boundary
 
     if not face_boundary:
         return 0
 
-    d = triangle_sign(v1, v2, v3, origin)
-    if not d:
-        raise ValueError("Face contains origin")
-
-    # d * [D] or d * [U], depending on sign of d * face_boundary.
-    return d if d * face_boundary > 0 else 0
+    ts = m12_01 * m3_2 + m23_01 * m1_2 + m31_01 * m2_2
+    if not ts:
+        raise ValueError("v1, v2, v3, origin are coplanar")
+    if (face_boundary > 0) == (ts > 0):
+        return face_boundary
+    else:
+        return 0
 
 
 class Polyhedron(object):
